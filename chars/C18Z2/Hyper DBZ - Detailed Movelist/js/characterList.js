8a085c2a87dcfb69251f238ab5e056ea7bcbefed
Villain.In.Glasses <bi.shamon739@gmail.com>
2023-06-15 08:17:58 +0800

    • Added Android 16

---
+++ chars/C18Z2/Hyper DBZ - Detailed Movelist/js/characterList.js
@@ -0,0 +1,21 @@
+	// Create an object for each character
+	// ID should be the folder name in the CHARA folder
+	// Having the defaultChara property set to 'yes' will have that character up on intial load
+	window.characters = [
+							{id: '1general', name: 'General Gameplay Information',  defaultChara: 'yes'},
+							{id: 'babidi', name: 'Babidi'},
+							{id: 'buu', name: 'Super Buu'},
+							{id: 'freeza', name: 'Freeza'},
+							{id: 'eighteen', name: 'Eighteen'},
+							{id: 'gohan', name: 'Gohan'},
+							{id: 'goku', name: 'Goku'},
+							{id: 'ssjgoku', name: 'SSJ Goku'},
+							{id: 'gotenks', name: 'Gotenks'},
+							{id: 'krillin', name: 'Krillin'},			
+							{id: 'piccolo', name: 'Piccolo'}, 
+							{id: 'mrsatan', name: 'Mr. Satan'},
+							{id: 'tien', name: 'TenShinHan'},
+							{id: 'vegeta', name: 'Vegeta'},
+							{id: 'ssjvegeta', name: 'SSJ Vegeta'},
+							{id: 'mvegeta', name: 'Majin Vegeta'} 							
+						]
